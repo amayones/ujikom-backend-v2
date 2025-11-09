201f9d86ad664da267a30847b9afe230fe70ef98
amayones <mayonedrive1@gmail.com>
2025-11-09 21:56:45 +0700

    Fix CORS: allow frontend domain

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:3000',
+        'https://ac.amayones.my.id',
+        'http://ac.amayones.my.id',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
