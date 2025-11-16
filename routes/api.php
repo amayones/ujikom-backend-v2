08a8b388429c0c689b0a7d279ae3c6f2c6876e8d
amayones <mayonedrive1@gmail.com>
2025-11-17 00:01:30 +0700

    Add: Endpoint /update-films dan /check-films untuk update poster production

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,12 @@ Route::get('/studios', function() {
         'data' => \App\Models\Studio::all()
     ]);
 });
+Route::get('/check-films', function() {
+    return response()->json([
+        'success' => true,
+        'data' => \App\Models\Film::select('id', 'title', 'poster', 'trailer')->get()
+    ]);
+});
 Route::get('/prices', function() {
     return response()->json([
         'success' => true,
@@ -99,4 +105,52 @@ Route::post('/payment/callback', [PaymentController::class, 'callback'])->withou
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/payment/snap-token/{orderId}', [PaymentController::class, 'getSnapToken']);
     Route::get('/payment/status/{orderNumber}', [PaymentController::class, 'checkStatus']);
+});
+
+// Utility Routes (for production updates)
+Route::get('/update-films', function() {
+    $updates = [
+        'Deadpool & Wolverine' => [
+            'poster' => 'https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+            'trailer' => 'https://www.youtube.com/watch?v=73_1biulkYk'
+        ],
+        'Inside Out 2' => [
+            'poster' => 'https://image.tmdb.org/t/p/w500/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg',
+            'trailer' => 'https://www.youtube.com/watch?v=LEjhY15eCx0'
+        ],
+        'Dune: Part Two' => [
+            'poster' => 'https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg',
+            'trailer' => 'https://www.youtube.com/watch?v=Way9Dexny3w'
+        ],
+        'Wicked' => [
+            'poster' => 'https://image.tmdb.org/t/p/w500/c5Tqxeo1UpBvnAc3csUm7j3hlQl.jpg',
+            'trailer' => 'https://www.youtube.com/watch?v=6COmYeLsz4c'
+        ],
+        'Moana 2' => [
+            'poster' => 'https://image.tmdb.org/t/p/w500/yh64qw9mgXBvlaWDi7Q9tpUBAvH.jpg',
+            'trailer' => 'https://www.youtube.com/watch?v=hDZ7y8RP5HE'
+        ],
+        'Gladiator II' => [
+            'poster' => 'https://image.tmdb.org/t/p/w500/2cxhvwyEwRlysAmRH4iodkvo0z5.jpg',
+            'trailer' => 'https://www.youtube.com/watch?v=nkD35yv1RM0'
+        ]
+    ];
+    
+    $results = [];
+    foreach ($updates as $title => $data) {
+        $film = \App\Models\Film::where('title', $title)->first();
+        if ($film) {
+            $film->update($data);
+            $results[] = "Updated: {$title}";
+        } else {
+            $results[] = "Not found: {$title}";
+        }
+    }
+    
+    return response()->json([
+        'success' => true,
+        'message' => 'Films updated',
+        'results' => $results,
+        'all_films' => \App\Models\Film::select('id', 'title', 'poster', 'trailer')->get()
+    ]);
 });
